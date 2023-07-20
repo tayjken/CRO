9bc6522d3ccd37c84f6130531bd1192b40d8d982
tayjken <72711001+tayjken@users.noreply.github.com>
2023-07-20 14:50:04 -0400

    Initial commit

---
+++ src/components/SPChanges/SPChanges.tsx
@@ -0,0 +1,311 @@
+import { memo } from 'react';
+import type { FC } from 'react';
+
+import resets from '../_resets.module.css';
+import { FrameIcon } from './FrameIcon';
+import classes from './SPChanges.module.css';
+
+interface Props {
+  className?: string;
+}
+/* @figmaId 1:2 */
+export const SPChanges: FC<Props> = memo(function SPChanges(props = {}) {
+  return (
+    <div className={`${resets.clapyResets} ${classes.root}`}>
+      <div className={classes.header}>
+        <div className={classes.divRow}>
+          <div className={classes.divColLg4}></div>
+          <div className={classes.divColLg42}></div>
+        </div>
+        <div className={classes.linkUsaPhoneOrg866372619}>866-372-0619</div>
+        <div className={classes.navLinkUsaPhoneOrgUSALogo_1Png}></div>
+      </div>
+      <div className={classes.section}>
+        <div className={classes.divMainBg}>
+          <div className={classes.divColLg5}>
+            <div className={classes.formApplication}>
+              <div className={classes.divContent}>
+                <div className={classes.tabpanel}>
+                  <div className={classes.divRow2}>
+                    <div className={classes.divMb3}>
+                      <div className={classes.input}>
+                        <div className={classes.divPlaceholder}>
+                          <div className={classes.firstName}>First Name*</div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className={classes.divMb32}>
+                      <div className={classes.input2}>
+                        <div className={classes.divPlaceholder2}>
+                          <div className={classes.lastName}>Last Name*</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className={classes.divRow3}>
+                    <div className={classes.divMb33}>
+                      <div className={classes.input3}>
+                        <div className={classes.divPlaceholder3}>
+                          <div className={classes.mobileNumber}>Mobile Number*</div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className={classes.divMb34}>
+                      <div className={classes.input4}>
+                        <div className={classes.divPlaceholder4}>
+                          <div className={classes.email}>Email*</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className={classes.divRow4}>
+                    <div className={classes.input5}>
+                      <div className={classes.divPlaceholder5}>
+                        <div className={classes.zipcode}>Zipcode*</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={classes.menuItemPaginationItem}>
+                <div className={classes.menuitemGETITNOW}>GET IT NOW</div>
+              </div>
+            </div>
+            <div className={classes.divColLg12}>
+              <div className={classes.divFormContainer}></div>
+              <div className={classes.heading31000AlreadyEnrolled}>
+                <p className={classes.labelWrapper}>
+                  <span className={classes.label}>100,000 +</span>
+                  <span className={classes.label2}> </span>
+                </p>
+                <div className={classes.textBlock}>Already Enrolled</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.Get100FREEUNLIMITED5GBData}>
+          <p className={classes.labelWrapper2}>
+            <span className={classes.label3}>+ Get 100% </span>
+            <span className={classes.label4}>FREE UNLIMITED</span>
+            <span className={classes.label5}> 5GB Data</span>
+          </p>
+        </div>
+        <div className={classes.separator}></div>
+        <div className={classes.cLAIMYOURTABLETWithACPProgram}>
+          <p className={classes.labelWrapper3}>
+            <span className={classes.label6}>CLAIM YOUR </span>
+            <span className={classes.label7}>TABLET</span>
+          </p>
+          <div className={classes.textBlock2}>
+            <p className={classes.labelWrapper4}>
+              <span className={classes.label8}>With ACP Program</span>
+            </p>
+          </div>
+        </div>
+        <div className={classes.myProject1}></div>
+        <div className={classes.divBgFull}>
+          <div className={classes.divRow5}>
+            <div className={classes.divColLg3}>
+              <div className={classes.pReviewTest}>
+                <div className={classes.IGotMineYesterdayILoveItMyTabl}>
+                  <div className={classes.textBlock3}>&quot;I got mine yesterday. I love it my</div>
+                  <div className={classes.textBlock4}>tablet. It works really good. I&#39;m</div>
+                  <div className={classes.textBlock5}>very happy that they have this</div>
+                  <div className={classes.textBlock6}>stuff. For people who are on</div>
+                  <div className={classes.textBlock7}>government programs&quot;</div>
+                </div>
+              </div>
+              <div className={classes.Michelle}>- Michelle</div>
+            </div>
+            <div className={classes.divColLg32}>
+              <div className={classes.pReviewTest2}></div>
+            </div>
+            <div className={classes.Linda}>- Linda</div>
+            <div className={classes.IGotMineOnThe3rdOfJuly2023Love}>
+              <div className={classes.textBlock8}>&quot;I got mine on the 3rd of July</div>
+              <div className={classes.textBlock9}>2023 love it &quot;</div>
+            </div>
+            <div className={classes.divColLg33}>
+              <div className={classes.pReviewTest3}></div>
+            </div>
+            <div className={classes.Jessie}>- Jessie</div>
+            <div className={classes.IfYouHaveSnapMedicaidMedicareO}>
+              <div className={classes.textBlock10}>&quot;if you have snap, medicaid,</div>
+              <div className={classes.textBlock11}>medicare or on social security</div>
+              <div className={classes.textBlock12}>you can get it. You get free</div>
+              <div className={classes.textBlock13}>internet also...go for it it</div>
+              <div className={classes.textBlock14}>government approved&quot;</div>
+            </div>
+            <div className={classes.divColLg34}>
+              <div className={classes.pReviewTest4}></div>
+            </div>
+            <div className={classes.John}>- John</div>
+            <div className={classes.IVeHadMineOverAYearNowOutstand}>
+              <div className={classes.textBlock15}>&quot;I&#39;ve had mine over a year now!!!</div>
+              <div className={classes.textBlock16}>Outstanding...&quot;</div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.link}>
+          <div className={classes.heading2APPLYNOW}>APPLY NOW</div>
+          <div className={classes.fillOutThisQuickFormToQualify}>Fill out this quick form to qualify</div>
+        </div>
+        <div className={classes.iMPORTANTAVerificationOfEnroll}>
+          IMPORTANT: A verification of enrollment will be sent to you by text. Please verify your mobile number is
+          correct
+        </div>
+        <div className={classes.frame1}>
+          <div className={classes.divColLg6}></div>
+          <div className={classes.divSimpleText}>
+            <div className={classes.pTextJustify}>
+              <div className={classes.theAffordableConnectivityProgr}>
+                The Affordable Connectivity Program (ACP) is a new government
+              </div>
+              <div className={classes.benefitProgramDesignedToMakeSu}>
+                <div className={classes.textBlock17}>
+                  benefit program designed to make sure everyone is online. If you
+                </div>
+                <div className={classes.textBlock18}>
+                  <p className={classes.labelWrapper5}>
+                    <span className={classes.label9}>are currently on one of these government benefits, </span>
+                    <span className={classes.label10}>you qualify</span>
+                    <span className={classes.label11}> for</span>
+                  </p>
+                </div>
+                <div className={classes.textBlock19}>this Free service and tablet program.</div>
+                <div className={classes.textBlock20}>
+                  <p></p>
+                </div>
+                <div className={classes.textBlock21}>
+                  <p className={classes.labelWrapper6}>
+                    <span className={classes.label12}> </span>
+                    <span className={classes.label13}>✔️ Food Stamps/SNAP</span>
+                  </p>
+                </div>
+                <div className={classes.textBlock22}>
+                  <p className={classes.labelWrapper7}>
+                    <span className={classes.label14}> </span>
+                    <span className={classes.label15}>✔️ Medicaid +</span>
+                  </p>
+                </div>
+                <div className={classes.textBlock23}> ✔️ Social Security/SSI</div>
+                <div className={classes.textBlock24}> ✔️ Federal Housing</div>
+                <div className={classes.textBlock25}> ✔️ Federal Pell Grants</div>
+                <div className={classes.textBlock26}> ✔️ Veteran’s Pension</div>
+                <div className={classes.textBlock27}> ✔️ Tribal Benefits</div>
+                <div className={classes.textBlock28}> ✔️ WIC</div>
+                <div className={classes.textBlock29}> ✔️ Yearly income less than $60,000</div>
+              </div>
+            </div>
+            <div className={classes.link2}>
+              <div className={classes.gETITNOW}>GET IT NOW</div>
+            </div>
+            <div className={classes.separator2}></div>
+          </div>
+          <div className={classes.heading2WhatIsACPAndHowDoIQual}>What is ACP and How do I qualify?</div>
+          <div className={classes.divColLg62}>
+            <div className={classes.heading2QUICKANDSIMPLESAVETIME}>QUICK AND SIMPLE - SAVE TIME &amp; MONEY</div>
+            <div className={classes.list}>
+              <div className={classes.item}>
+                <div className={classes.divUsicon1}></div>
+                <div className={classes.p}>
+                  <div className={classes.strongENROLL}>ENROLL</div>
+                  <div className={classes.fillOutYourInformationAndClick}>
+                    Fill out your information and click submit!
+                  </div>
+                </div>
+              </div>
+              <div className={classes.item2}>
+                <div className={classes.divUsicon2}></div>
+                <div className={classes.p2}>
+                  <div className={classes.strongVERIFICATION}>VERIFICATION</div>
+                  <div className={classes.receiveEmailConfirmationOfAppr}>
+                    <div className={classes.textBlock30}>Receive email confirmation of Approved Enrollment &amp;</div>
+                    <div className={classes.textBlock31}>shipping confirmation!</div>
+                  </div>
+                </div>
+              </div>
+              <div className={classes.item3}>
+                <div className={classes.divUsicon3}></div>
+                <div className={classes.p3}>
+                  <div className={classes.strongACTIVATEUSE}>ACTIVATE &amp; USE</div>
+                  <div className={classes.thatSItThatSimpleWhenYouReceiv}>
+                    <div className={classes.textBlock32}>
+                      That&#39;s it, That Simple. When you receive the tablet, activate
+                    </div>
+                    <div className={classes.textBlock33}>and enjoy your free internet!</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={classes.link3}>
+              <div className={classes.aPPLYNOW}>APPLY NOW</div>
+            </div>
+            <div className={classes.divColLg63}>
+              <div className={classes.heading2}>
+                <div className={classes.hereSWhy10000UsersHaveSignedUp}>
+                  Here&#39;s Why 100,000+ Users Have Signed Up
+                </div>
+                <div className={classes.withUSAPhone}>With USAPhone</div>
+              </div>
+              <div className={classes.divSimpleText3}>
+                <div className={classes.p4}>
+                  <div className={classes.strongFREEUNLIMITED5GBDATA}>
+                    <div className={classes.textBlock34}>FREE UNLIMITED 5GB DATA</div>
+                    <div className={classes.textBlock35}>
+                      <p className={classes.labelWrapper8}>
+                        <span className={classes.label16}>Data, Talk, &amp; Text</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className={classes.p5}>
+                  <div className={classes.strongSTAYCONNECTED}>
+                    <div className={classes.textBlock36}>STAY CONNECTED</div>
+                    <div className={classes.textBlock37}>
+                      <p className={classes.labelWrapper9}>
+                        <span className={classes.label17}>Use for School, Work, Family &amp; Friends</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className={classes.p6}>
+                  <div className={classes.strongTSAVEUPTO75AMONTH}>
+                    <div className={classes.textBlock38}>SAVE UP TO $75 A MONTH</div>
+                    <div className={classes.textBlock39}>
+                      <p className={classes.labelWrapper10}>
+                        <span className={classes.label18}>No Contract or Credit Check!</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={classes.separator3}></div>
+          </div>
+        </div>
+        <div className={classes.image1}></div>
+        <div className={classes.iframe}>
+          <div className={classes.sVG}>
+            <div className={classes.frame}>
+              <FrameIcon className={classes.icon} />
+            </div>
+          </div>
+        </div>
+        <div className={classes.frame2}></div>
+      </div>
+      <div className={classes.divFooterDark}>
+        <div className={classes.divColLg43}>
+          <div className={classes._2023ALLRIGHTSRESERVED}>2023. ALL RIGHTS RESERVED.</div>
+        </div>
+        <div className={classes.list2}>
+          <div className={classes.itemLinkTERMSOFSERVICE}>TERMS OF SERVICE</div>
+          <div className={classes.itemLinkPRIVACYPOLICY}>PRIVACY POLICY</div>
+          <div className={classes.itemLinkACPCONSENTFORM}>ACP CONSENT FORM</div>
+          <div className={classes.itemLinkCUSTOMERSUPPORT}>CUSTOMER SUPPORT</div>
+        </div>
+      </div>
+      <div className={classes.heading1FREEGOVERNMENTBENEFITP}>FREE GOVERNMENT BENEFIT PROGRAM</div>
+    </div>
+  );
+});
